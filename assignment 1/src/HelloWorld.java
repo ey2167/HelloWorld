123ce520d1bb0e9eb4217c781f1ad222576d53e0
ey2167 <ey2167@an098051-ct103v.nvstu.edu>
2014-08-25 15:05:55 -0400

    Initial

---
+++ assignment 1/src/HelloWorld.java
@@ -0,0 +1,11 @@
+
+import java.util.Scanner;
+
+public class HelloWorld {
+	
+	public static void main(String[]args){
+		Scanner scan = new Scanner(System.in);
+		System.out.println("hello world" + " " + scan.next());
+		
+	}
+}
